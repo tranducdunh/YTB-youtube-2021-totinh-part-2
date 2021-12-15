4cd90d05977d2ed6e346732fe018194f19b9800e
tranducdunh <96191692+tranducdunh@users.noreply.github.com>
2021-12-16 01:35:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     btnIntro: 'OK',
     title: 'Anh biết Em thích Anh phải không phải không ? 😙',
     desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
+    btnYes: 'Có, tớ thật sự thích cậu 💖💖💖',
+    btnNo: '💔 cậu mơ à ! 💔 ',
     question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
@@ -14,5 +14,4 @@ const CONFIG = {
     mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
     messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
     btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
-}
+    messLink: 'https://www.facebook.com/profile.php?id=100015663631965'
